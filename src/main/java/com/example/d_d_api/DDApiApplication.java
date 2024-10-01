1f4628229362cf26f9a5a5bccbc560fdcb0e8ccf
Gustavo Neskovek Goulart <gustavo.184217@alunosatc.edu.br>
2024-10-01 19:11:31 -0300

    fst commit

---
+++ src/main/java/com/example/d_d_api/DDApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.d_d_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DDApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DDApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/d_d_api/DDApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.d_d_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DDApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
